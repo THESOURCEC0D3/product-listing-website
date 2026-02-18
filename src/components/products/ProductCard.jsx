687b917fff6f627780698923ff3ac7371738f2c2
THESOURCEC0D3 <jonathantamunoiduari@gmail.com>
2026-02-18 16:04:08 +0100

    Making the texts colors black

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -6,12 +6,12 @@ const ProductCard = ({ product }) => {
   return (
     <div className="flex flex-col space-y-3 items-center border rounded-2xl shadow-2xl shadow-gray-500 p-4 hover:scale-110 hover:border-blue-400 hover:shadow-2xl hover:shadow-purple-500 bg-white transition-all duration-500 z-10 cursor-pointer">
       <img src={product.image} alt={product.name} className="size-52" />
-      <h2>{product.name}</h2>
-      <p>{product.price.toLocaleString()}</p>
-      <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
+      <h2 className="text-black">{product.name}</h2>
+      <p className="text-black">{product.price.toLocaleString()}</p>
+      <p className="text-black">{product.inStock ? "In Stock" : "Out of Stock"}</p>
       <a
         href={createWhatsAppOrderLink(product, 2348065429005)}
-        className="p-3 bg-gray-200 rounded-2xl"
+        className="p-3 text-black bg-gray-200 rounded-2xl"
       >
         Get Product
       </a>
