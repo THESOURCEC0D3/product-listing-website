9daf8ac5df6956298208a71dccd3b9942f6bfeda
THESOURCEC0D3 <jonathantamunoiduari@gmail.com>
2026-02-24 23:14:36 +0100

    I made some changes to the why choose us animation

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -41,44 +41,47 @@ const WhyChooseUs = () => {
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 80 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{duration: 0.8, ease: "easeOut"}}
-      viewport={{ once: true, amount: 0.1}}
-      className="bg-neutral-50 py-20 px-6"
-    >
-      <h2 className="text-4xl font-bold text-black text-center">
-        Why Choose Us
-      </h2>
-      <div className="max-w-7xl mx-auto text-center flex flex-col md:flex-row items-center gap-12 p-8">
-        <motion.div
+    <div className="bg-neutral-50 py-20 px-6">
+      <motion.div
         initial={{ opacity: 0, y: 80 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{duration: 0.8, ease: "easeOut"}}
-        viewport={{ once: true, amount: 0.1}}
-        className="flex-1">
-          <img src="/assets/WCU1.jpg" className="rounded-2xl"></img>
-        </motion.div>
-        <motion.div
-        variants={containerVariants}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true, amount: 0.1}}
-        className="grid md:grid-cols-1 gap-8 bg-amber-300-50 p-8 rounded-2xl flex-1">
-          {features.map((item, index) => (
-            <motion.div
-              key={index}
-              variants={itemVariants}
-              className="bg-white p-8 rounded-2xl shadow-sm hover:shadow-lg transition duration-300 cursor-pointer size-full"
-            >
-              <h3 className="text-xl font-semibold mb-3">{item.title}</h3>
-              <p className="text-gray-600 text-sm">{item.desc}</p>
-            </motion.div>
-          ))}
-        </motion.div>
-      </div>
-    </motion.div>
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ once: true, amount: 0.1 }}
+      >
+        <h2 className="text-4xl font-bold text-black text-center">
+          Why Choose Us
+        </h2>
+        <div className="max-w-7xl mx-auto text-center flex flex-col md:flex-row items-center gap-12 p-8">
+          <motion.div
+            initial={{ opacity: 0, y: 80 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.1 }}
+            className="flex-1"
+          >
+            <img src="/assets/WCU1.jpg" className="rounded-2xl"></img>
+          </motion.div>
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.1 }}
+            className="grid md:grid-cols-1 gap-8 bg-amber-300-50 p-8 rounded-2xl flex-1"
+          >
+            {features.map((item, index) => (
+              <motion.div
+                key={index}
+                variants={itemVariants}
+                className="bg-white p-8 rounded-2xl shadow-sm hover:shadow-lg transition duration-300 cursor-pointer size-full"
+              >
+                <h3 className="text-xl font-semibold mb-3">{item.title}</h3>
+                <p className="text-gray-600 text-sm">{item.desc}</p>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </motion.div>
+    </div>
   );
 }
 
